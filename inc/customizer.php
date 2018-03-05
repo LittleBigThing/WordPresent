05fb060388575402f69a3a69e2309860f79d5f6c
Csaba <Csaba@csabas-mbp.home>
2018-03-05 10:55:17 +0100

    re-activate postMessage transport to header_textcolor setting

--- inc/customizer.php
+++ inc/customizer.php
@@ -13,7 +13,7 @@
 function wordpresent_customize_register( $wp_customize ) {
 	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
-	//$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
 	$wp_customize->remove_control( 'header_textcolor' );
 
 	if ( isset( $wp_customize->selective_refresh ) ) {
